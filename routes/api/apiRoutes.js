5a2fd1ee4e749c9d6542304663d7cf582caabd83
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-05 14:25:19 -0400

    added route to get all notes

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -3,3 +3,7 @@ const { notes } = require("../../db/db.json");
 const { validateNote, createNewNote } = require("../../lib/notes");
 const router = require("express").Router();
 
+// Get all notes
+router.get("/notes", (req, res) => {
+    res.json(notes);
+  });
