9034ac0b80175c39a4eac4785702fd8493f2842a
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-05 14:32:12 -0400

    added post route to create a new note, exported router

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -7,3 +7,16 @@ const router = require("express").Router();
 router.get("/notes", (req, res) => {
     res.json(notes);
   });
+
+// Create new notes
+router.post("/notes", (req, res) => {
+    req.body.id = notes.length.toString();
+    if (!validateNote(req.body)) {
+      res.status(400).send("Please format your note correctly.");
+    } else {
+      const newNote = createNewNote(req.body, notes);
+      res.json(newNote);
+    }
+  });
+  
+  module.exports = router;
