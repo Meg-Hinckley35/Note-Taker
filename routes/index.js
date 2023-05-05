eb0e1b4be943de2d975085127531b846431f7a63
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-05 14:40:08 -0400

    set up routes to follow a path to our html files

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+// require express router and path
+const path = require("path");
+const router = require("express").Router();
+
+// set routes to HTML files
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
+  });
+  
+  router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/notes.html"));
+  });
+  
+  module.exports = router;
